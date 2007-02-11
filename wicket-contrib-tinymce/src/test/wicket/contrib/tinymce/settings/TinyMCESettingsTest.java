79497efbf1ea41ebe8b3332ba4e2ed5911f6ce7d
Iulian Costan <syca@wicketstuff.org>
2007-02-11 11:24:31 +0000

    add language pack
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/WICKET_1_2@1750 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-tinymce/src/test/wicket/contrib/tinymce/settings/TinyMCESettingsTest.java
+++ wicket-contrib-tinymce/src/test/wicket/contrib/tinymce/settings/TinyMCESettingsTest.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 import junit.framework.TestCase;
 import wicket.Component;
+import wicket.contrib.tinymce.settings.Plugin;
 import wicket.contrib.tinymce.settings.SavePlugin;
 import wicket.contrib.tinymce.settings.DateTimePlugin;
 import wicket.contrib.tinymce.settings.TinyMCESettings;
@@ -139,6 +140,8 @@ public class TinyMCESettingsTest extends TestCase
 	{
 		Plugin plugin = new Plugin("mockplugin")
 		{
+			private static final long serialVersionUID = 1L;
+
 			protected void definePluginExtensions(StringBuffer buffer)
 			{
 				buffer.append("alert('Hello Mock World');");
@@ -152,14 +155,15 @@ public class TinyMCESettingsTest extends TestCase
 		assertEquals("alert('Hello Mock World');", javascript);
 	}
 
-	public testExactMode()
+	public void testExactMode()
 	{
-		Component c1 = new TextArea();
-		
+		TextArea textArea = new TextArea("ta");
+
 		TinyMCESettings settings = new TinyMCESettings(Mode.exact);
-		settings.addComponent(c2);
+		settings.enableTextArea(textArea);
 		String js = settings.toJavaScript();
-		//TODO implemnet me
+
+		// TODO implemnet me
 		assertNotNull(js);
 	}
 }
