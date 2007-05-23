8dc2beff64277c53ac32aa97b3ed036d5bf4bfdc
Nino Martinez Wael <nmwael@wicketstuff.org>
2007-05-23 12:12:43 +0000

    wrong check
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/WICKET_1_2@2199 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-gmap/src/java/wicket/contrib/gmap/GMarkerComponent.java
+++ wicket-contrib-gmap/src/java/wicket/contrib/gmap/GMarkerComponent.java
@@ -66,7 +66,7 @@ class GMarkerComponent extends JavaScriptComponent
 				+ icon.getAnchor().toString() + ";\n" + "icon.infoWindowAnchor = "
 				+ icon.getInfoWindowAncor().toString() + "\n";
 		String customIconPartTwo = "";
-		if (gmarker.getToolTip() == null && gmarker.getToolTip().length() > 0)
+		if (gmarker.getToolTip() != null && gmarker.getToolTip().length() > 0)
 		{
 			customIconPartTwo = "var marker = new GMarker(" + gmarker.getPointAsString()
 					+ ",{icon:icon, title:'" + gmarker.getToolTip() + "'});" + "\n"
