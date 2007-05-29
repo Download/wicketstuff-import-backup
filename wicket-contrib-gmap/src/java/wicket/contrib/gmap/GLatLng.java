1093920fee0f9808c1c66ac8763792dcb2b41d53
Nino Martinez Wael <nmwael@wicketstuff.org>
2007-05-29 10:40:20 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/WICKET_1_2@2243 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-gmap/src/java/wicket/contrib/gmap/GLatLng.java
+++ wicket-contrib-gmap/src/java/wicket/contrib/gmap/GLatLng.java
@@ -23,6 +23,9 @@ import java.io.Serializable;
  * Represents Googles GLatLng class contains Geo location stored in latitude and
  * longtitude
  * 
+ * Latitude will be clamped to lie within [-90,90] and longitude will be wrapped to
+ * lie within ]-180, 180]
+ * 
  * @author Nino Martinez Wael
  */
 public class GLatLng implements Serializable
@@ -50,8 +53,8 @@ public class GLatLng implements Serializable
 	 */
 	public GLatLng(double latitude, double longitude)
 	{
-		this.longitude = longitude;
-		this.latitude = latitude;
+		setLatitude(latitude);
+		setLongitude(longitude);
 	}
 
 	/**
@@ -82,28 +85,34 @@ public class GLatLng implements Serializable
 
 	public void setLatitude(double latitude)
 	{
+		//clamp latitude
+		if (latitude < -90)
+			latitude = -90;
+		if (latitude > 90)
+			latitude = 90;
+
 		this.latitude = latitude;
 	}
 
 	public void setLongitude(double longitude)
 	{
+		//wrap longitude
+		longitude = longitude%360;
+		
+		if (longitude > 180) {
+			longitude = longitude - 360;
+		} else if (longitude <= -180){
+			longitude = longitude + 360;
+		}
+		
 		this.longitude = longitude;
 	}
 
-	/**
-	 * @param gLatLng
-	 * @return distance from this coordinate to another GLatLng coordinate in meters.
-	 * {@linkplain} http://www.pk.org/rutgers/hw/a-5.html
-	 * {@linkplain} http://www.cs.rutgers.edu/~pxk/rutgers/hw/a-5.html
-	 * @author Ryan Gravener
-	 */
-	public double distance(final GLatLng gLatLng)
+	public double distance(GLatLng other)
 	{
-		return Math.toDegrees(Math.acos(Math.sin(Math.toRadians(this.latitude)) *
-				Math.sin(Math.toRadians(gLatLng.latitude)) +
-				Math.cos(Math.toRadians(this.latitude)) *
-				Math.cos(Math.toRadians(gLatLng.latitude)) * 
-				Math.cos(Math.toRadians(this.longitude-gLatLng.longitude)))) * 60 * 1852;
+		double la = Math.abs(latitude - other.getLatitude());
+		double ln = Math.abs(longitude - other.getLongitude());
+		return Math.max(la, ln);
 	}
 
 
