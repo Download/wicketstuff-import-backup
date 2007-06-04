8ac1c3d0f4b34666b862ec0735d8b8a8e89166c8
Nino Martinez Wael <nmwael@wicketstuff.org>
2007-06-04 09:50:43 +0000

    need to be able to update a point on a overlay.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/WICKET_1_2@2322 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wicket-contrib-gmap/src/java/wicket/contrib/gmap/Overlay.java
+++ wicket-contrib-gmap/src/java/wicket/contrib/gmap/Overlay.java
@@ -1,20 +1,19 @@
 /*
- * $Id$
- * $Revision$
- * $Date$
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
+ * $Id$ $Revision$ $Date:
+ * 2007-05-10 11:29:11 +0200 (Thu, 10 May 2007) $
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package wicket.contrib.gmap;
 
@@ -26,41 +25,47 @@ import java.io.Serializable;
 public abstract class Overlay implements Serializable
 {
 
-    private GLatLng point;
+	private GLatLng point;
 
-    Overlay(GLatLng point)
-    {
-        if (point == null)
-        {
-            new IllegalArgumentException("point cannot be null");
-        }
-        this.point = point;
-    }
+	Overlay(GLatLng point)
+	{
+		if (point == null)
+		{
+			new IllegalArgumentException("point cannot be null");
+		}
+		this.point = point;
+	}
 
-    /**
-     * Get the name of the JavaScript function that will be called in order to create this overlay.
-     *
-     * @return name of JavaScript function
-     */
-    public abstract String getFactoryMethod();
+	/**
+	 * Get the name of the JavaScript function that will be called in order to
+	 * create this overlay.
+	 * 
+	 * @return name of JavaScript function
+	 */
+	public abstract String getFactoryMethod();
 
-    public GLatLng getPoint()
-    {
-        return point;
-    }
+	public GLatLng getPoint()
+	{
+		return point;
+	}
 
-    public String getPointAsString()
-    {
-        return point.toString();
-    }
+	public void setPoint(GLatLng gLatLng)
+	{
+		point = gLatLng;
+	}
 
-    /**
-     * Each overlay on the map has to be unique.
-     *
-     * @return unique ID for each overlay
-     */
-    protected String getOverlayId()
-    {
-        return JSUtil.longitudeAsString(point) + JSUtil.latitudeAsString(point);
-    }
+	public String getPointAsString()
+	{
+		return point.toString();
+	}
+
+	/**
+	 * Each overlay on the map has to be unique.
+	 * 
+	 * @return unique ID for each overlay
+	 */
+	protected String getOverlayId()
+	{
+		return JSUtil.longitudeAsString(point) + JSUtil.latitudeAsString(point);
+	}
 }
